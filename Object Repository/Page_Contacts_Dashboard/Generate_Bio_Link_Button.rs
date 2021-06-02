397967047c3b0f30e9e22c3cc6fcdbe0c45227c5
Juan Sánchez <judasane@gmail.com>
2021-06-01 20:30:57 -0500

    test for bio link with 'no' answer

---
+++ Object Repository/Page_Contacts_Dashboard/Generate_Bio_Link_Button.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Generate_Bio_Link_Button</name>
+   <tag></tag>
+   <elementGuidId>dbfafcb5-4462-4b50-bf50-b5096b0c73be</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[text() = &quot;introduced.omr@gmail.com&quot;]/parent::*/parent::*/parent::*/*[@class=&quot;contacts-list_start-intro&quot;]/button[text() = &quot;Generate Bio Link&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
